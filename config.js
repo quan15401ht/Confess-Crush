e01cb0b0d3f59bd4de232d0f68a8327b8631b2bc
quan15401ht <115921594+quan15401ht@users.noreply.github.com>
2022-10-16 15:47:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Beoiucuaa",
+    introTitle: 'Phạm Thị Hương Thảo',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
